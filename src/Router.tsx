8654d6ea702dc25532a150c1ea4221cb40df8d22
YasSayuri <122479117+yasSayuri@users.noreply.github.com>
2025-04-18 17:43:59 -0300

    gracinha

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 import { Login } from './pages/login';
 import { Cadastro } from './pages/cadastro';
+import { Web } from './pages/web';
 
 export function Router() {
   return (
     <Routes> 
       <Route path="/" element={<Login />}></Route>
       <Route path='/cadastro' element={<Cadastro />}></Route>
+      <Route path='/web' element={<Web />}></Route>
     </Routes>
   )
 }
---
+++ src/pages/web/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Web() {
+    return(
+        <h1>pagina principal</h1>
+    )
+}
